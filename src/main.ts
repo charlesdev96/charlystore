f0f3bce74698daf2ae0539d0bc36fd451406c195
charlesdev96 <ogonnayacn@gmail.com>
2025-06-21 20:13:56 +0100

    webhook

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { AllExceptionsFilter } from "./common/error/globalErrorFilter.error";
+import helmet from "helmet";
+import * as compression from "compression";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors({
+    origin: "*", // or specify trusted origins
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  });
+  app.use(compression());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //strips properties that dont have decorators
